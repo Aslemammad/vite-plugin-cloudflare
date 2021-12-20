8bb068979f36473373ca7dabc905a6413cd06d36
aslemammad <zorofight94@gmail.com>
2021-12-20 16:19:58 +0330

    some modifications

--- packages/vite-plugin-cloudflare/src/cli.ts
+++ packages/vite-plugin-cloudflare/src/cli.ts
@@ -9,32 +9,41 @@ cli
   .version(version)
   .command("build <input>", "build worker")
   .option("--outputDir <outputDir>", "output directory")
-  .action(async (input: string, options: { outputDir?: string }) => {
-    const { build } = await import("vite");
-    const outputDir = options.outputDir || path.dirname(input);
+  .option("--outputFile <outputDir>", "output file name without extension")
+  .action(
+    async (
+      input: string,
+      options: { outputDir?: string; outputFile?: string }
+    ) => {
+      const { build } = await import("vite");
 
-    await build({
-      mode: "production",
-      // @ts-ignore
-      ssr: { noExternal: true },
-      optimizeDeps: {
-        keepNames: undefined,
-      },
-      build: {
-        outDir: path.join(root, outputDir),
-        brotliSize: true,
-        ssr: true,
-        // minify: true,
-        rollupOptions: {
-          input,
-          output: {
-            format: "esm",
+      const outputDir = options.outputDir || path.dirname(input);
+
+      const entries = { [options.outputFile || path.parse(input).name]: input };
+
+      await build({
+        configFile: false,
+        plugins: [],
+        mode: "production",
+        // @ts-ignore
+        // ssr: { noExternal: true },
+        build: {
+          emptyOutDir: false,
+          outDir: path.join(root, outputDir),
+          brotliSize: true,
+          ssr: true,
+          // minify: true,
+          rollupOptions: {
+            input: entries,
+            output: {
+              format: "esm",
+            },
           },
+          polyfillDynamicImport: false,
         },
-        polyfillDynamicImport: false,
-      },
-    });
-  });
+      });
+    }
+  );
 
 cli.help();
 cli.parse();
