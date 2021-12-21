59981bfd380edd81d9bd509b723887c1516ffb90
aslemammad <zorofight94@gmail.com>
2021-12-21 11:47:14 +0330

    use esbuild + shims

--- packages/vite-plugin-cloudflare/src/cli.ts
+++ packages/vite-plugin-cloudflare/src/cli.ts
@@ -1,6 +1,9 @@
+import esbuild from "esbuild";
 import path from "path";
 import cac from "cac";
 import { version } from "../package.json";
+import { plugin } from "./plugin";
+import { getShim } from "./shim";
 
 const cli = cac("vite-plugin-cloudflare");
 const root = process.cwd();
@@ -15,32 +18,37 @@ cli
       input: string,
       options: { outputDir?: string; outputFile?: string }
     ) => {
-      const { build } = await import("vite");
-
       const outputDir = options.outputDir || path.dirname(input);
+      console.log(esbuild.build);
 
-      const entries = { [options.outputFile || path.parse(input).name]: input };
-
-      await build({
-        configFile: false,
-        plugins: [],
-        mode: "production",
-        // @ts-ignore
-        // ssr: { noExternal: true },
-        build: {
-          emptyOutDir: false,
-          outDir: path.join(root, outputDir),
-          brotliSize: true,
-          ssr: true,
-          // minify: true,
-          rollupOptions: {
-            input: entries,
-            output: {
-              format: "esm",
-            },
-          },
-          polyfillDynamicImport: false,
+      const outFile = path.join(outputDir, options.outputFile || path.parse(input).name) + '.js'
+      await esbuild.build({
+        banner: {
+          js: getShim(path.join(root, outputDir, (options.outputFile || '') + '.js')),
         },
+        plugins: [plugin],
+        platform: "node",
+        outfile: outFile,
+        format: "esm",
+        entryPoints: [input],
+        // treeShaking: true,
+        sourcemap: "inline",
+        bundle: true,
+        write: true,
+        // plugins: [moduleLoader],
+      });
+      await esbuild.build({
+        allowOverwrite: true,
+        plugins: [plugin],
+        platform: "node",
+        outfile: outFile,
+        format: "esm",
+        entryPoints: [outFile],
+        // treeShaking: true,
+        sourcemap: "inline",
+        bundle: true,
+        write: true,
+        // plugins: [moduleLoader],
       });
     }
   );
