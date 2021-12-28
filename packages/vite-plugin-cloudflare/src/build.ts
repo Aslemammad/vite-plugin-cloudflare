135aff67170da1302b4cc869af2f17709cce3412
aslemammad <zorofight94@gmail.com>
2021-12-28 21:05:15 +0330

    dev mode

--- packages/vite-plugin-cloudflare/src/build.ts
+++ packages/vite-plugin-cloudflare/src/build.ts
@@ -5,29 +5,34 @@ import { fileURLToPath, resolve } from "mlly";
 import { plugin } from "./plugin";
 
 const root = process.cwd();
-export async function build(
-  options: Partial<esbuild.BuildOptions> & { outfile: string }
-) {
+export async function build(options: {
+  output: string;
+  input: string;
+  incremental: boolean;
+  debug: boolean;
+}) {
   const shimFile = fileURLToPath(
     await resolve("vite-plugin-cloudflare/shimmed")
   );
   return await esbuild.build({
+    outfile: options.output,
+    entryPoints: [options.input],
+    incremental: options.incremental,
+    logLevel: options.debug ? "debug" : "info",
     banner: {
       js: `${await readFile(shimFile, "utf8")}\n
-            globalThis.__filename = "${path.join(root, options.outfile)}";
+            globalThis.__filename = "${path.join(root, options.output)}";
             globalThis.__dirname = "${path.dirname(
-              path.join(root, options.outfile)
+              path.join(root, options.output)
             )}";
 
         `,
     },
     plugins: [plugin],
     platform: "node",
-    outfile: options.outfile,
     format: "esm",
+    target: 'es2020',
     loader: {},
-    entryPoints: options.entryPoints,
-    sourcemap: "inline",
     bundle: true,
     write: true,
     minify: true,
--- packages/vite-plugin-cloudflare/src/cli.ts
+++ packages/vite-plugin-cloudflare/src/cli.ts
@@ -1,63 +1,106 @@
-import esbuild from "esbuild";
-import path from "path";
+import { join } from "path";
 import cac from "cac";
 import c from "picocolors";
 import { version } from "../package.json";
-import { plugin } from "./plugin";
-import { resolve, fileURLToPath } from "mlly";
-import { dirname } from "path";
-import { readFile } from "fs/promises";
-import { createServer, ViteDevServer } from "vite";
+import { createServer, ViteDevServer, Plugin } from "vite";
 import { build } from "./build";
+import { ConsoleLog, Miniflare } from "miniflare";
 
 const cli = cac("vite-plugin-cloudflare");
-const root = process.cwd();
 
 cli
   .command("build <input> <output>", "build worker")
-  .action(async (input: string, output: string) => {
-    try {
-      console.log(c.green(`Building ${c.bold(input)}`));
+  .option("-d, --debug", "enable debugging", { default: false })
+  .action(
+    async (input: string, output: string, options: { debug: boolean }) => {
+      try {
+        console.log(`Building ${c.cyan(c.bold(input))}`);
 
-      await build({
-        outfile: output,
-        entryPoints: [input],
-      });
+        await build({
+          output,
+          input,
+          incremental: false,
+          debug: options.debug,
+        });
 
-      console.log(c.green(`Built ${c.bold(output)}`));
-    } catch (e) {
-      console.error(c.red("Failed to build. \n" + String(e)));
-      process.exit(1);
+        console.log(`Built ${c.cyan(c.bold(output))}`);
+      } catch (e) {
+        console.error(c.red("Failed to build. \n" + String(e)));
+        process.exit(1);
+      }
     }
-  });
+  );
 
 cli
   .command("dev <input> <output>")
-  .action(async (input: string, output: string) => {
-    console.log("hello");
-    let server: ViteDevServer;
-    try {
-      server = await createServer({
-        root: process.cwd(),
-        mode: "dev",
+  .option("-p, --port <input>", "miniflare port", { default: 3000 })
+  .option("-d, --debug", "enable debugging", { default: false })
+  .action(
+    async (
+      input: string,
+      output: string,
+      options: { port: number; debug: boolean }
+    ) => {
+      const { rebuild } = await build({
+        output,
+        input,
+        incremental: true,
+        debug: options.debug,
+      });
+      console.log(`Built ${c.cyan(c.bold(output))}`);
 
-        server: {
-          watch: {},
-          hmr: true,
-        },
-        plugins: [
-          {
-            name: "vite-plugin-cloudflare",
-            handleHotUpdate({ file }) {
-              console.log("here", file);
-            },
-          },
-        ],
+      let server: ViteDevServer;
+      const miniflare = new Miniflare({
+        watch: true,
+        port: options.port,
+        log: options.debug ? new ConsoleLog(true) : false,
       });
 
-      server.listen(3000);
-    } catch (e: any) {}
-  });
+      try {
+        miniflare.createServer().listen(options.port);
+
+        const root = process.cwd();
+        server = await createServer({
+          root,
+          mode: "dev",
+          logLevel: options.debug ? "info" : "silent",
+          server: {
+            hmr: true,
+          },
+          plugins: [
+            {
+              name: "vite-plugin-cloudflare",
+              async handleHotUpdate({ file }) {
+                if (file === join(root, output)) {
+                  return;
+                }
+                const start = performance.now();
+                await rebuild?.();
+                await miniflare.reloadOptions();
+                console.log(
+                  `Reloading ${c.cyan(c.bold(file))}`,
+                  c.gray(`${(performance.now() - start).toFixed(2)}ms`)
+                );
+              },
+            } as Plugin,
+          ],
+        });
+
+        await server.listen();
+        console.log(
+          `Listening on ${c.cyan(
+            `http://localhost:${options.port}`
+          )} for Miniflare, ${c.cyan(
+            `http://localhost:${server.config.server.port}`
+          )} for Vite`
+        );
+      } catch (e: any) {}
+      process.on("beforeExit", () => {
+        rebuild?.dispose();
+        miniflare.dispose();
+      });
+    }
+  );
 
 cli.version(version);
 cli.help();
