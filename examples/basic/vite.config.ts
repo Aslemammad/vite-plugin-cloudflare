b3c07dc9673035dfaf25f8b7a4746a112d35ca88
M. Bagher Abiat <zorofight94@gmail.com>
2022-12-30 11:56:06 +0330

    update basic example

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 import vpc from "vite-plugin-cloudflare";
 
 export default defineConfig({
+  esbuild: {
+    define: {
+      DEBUG: `${process.env.NODE_ENV === "development"}`,
+    }
+  },
   plugins: [vpc({ scriptPath: "./worker/index.ts" })],
 });
--- examples/basic/worker/index.ts
+++ examples/basic/worker/index.ts
@@ -1,28 +1,32 @@
 import { endianness } from "os";
+import { handleStaticAssets } from "./static-assets";
 
-addEventListener("fetch", (event) => {
+declare const DEBUG: boolean;
+
+addEventListener("fetch", async (event) => {
   const { pathname } = new URL(event.request.url);
   if (pathname.startsWith("/api")) {
     event.respondWith(handleRequest(event.request));
     return;
   }
-  // we skip miniflare and let vite handle the url
-  event.respondWith(new Response("", { headers: { "x-skip-request": "" } }));
+  if (DEBUG) {
+    // we skip miniflare and let vite handle the url
+    event.respondWith(new Response("", { headers: { "x-skip-request": "" } }));
+  } else {
+    // this will disable HMR in vite, so only for production
+    event.respondWith(handleStaticAssets(event));
+  }
 });
 
 async function handleRequest() {
   const obj = {
-    __dirname: __dirname,
-    __filename: __filename,
+    "__dirname": __dirname,
+    "__filename": __filename,
     cwd: process.cwd(),
     global: !!global,
     Buffer: !!Buffer,
     process: !!process,
-    endianness: !!endianness,
-    // TODO: Don't work properly
-    /* XMLHttpRequest: !!XMLHttpRequest,
-    XMLHttpRequestUpload: !!XMLHttpRequestUpload,
-    XMLHttpRequestEventTarget: !!XMLHttpRequestEventTarget, */
+    endianness: !!endianness
   };
   return new Response(JSON.stringify(obj));
 }
